0d43385c7784d722ae0f978f33969b01354c19d8
Angela Vazquez <angyvazquezdel97@gmail.com>
2019-06-17 14:13:54 +0200

    junit hecho de obtenerEdad

--- Test/tests/TestGestion.java
+++ Test/tests/TestGestion.java
@@ -6,6 +6,9 @@ import gestion.Gestion;
 import org.junit.jupiter.api.Test;
 import utilidades.Utilidades;
 
+import java.util.GregorianCalendar;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestGestion {
@@ -13,10 +16,27 @@ public class TestGestion {
     void testObtenerEdad() {
         Gestion g = new Gestion();
         Utilidades u = new Utilidades();
+        GregorianCalendar fecha = new GregorianCalendar();
         UsuarioImpl usuario = u.cargarUsuario("randomkwiz", "123456789");
         int edad = g.obtenerEdad(usuario);
+        assertEquals(22,edad);
 
-        assertTrue(edad == 1);
+        fecha.set(GregorianCalendar.YEAR, 2003);
+        fecha.set(GregorianCalendar.MONTH, 12);
+        fecha.set(GregorianCalendar.DATE, 1);
+        usuario.setFechaNacimiento(fecha);
+
+        edad = g.obtenerEdad(usuario);
+        assertEquals(15,edad);
+
+
+        fecha.set(GregorianCalendar.YEAR, 2050);
+        fecha.set(GregorianCalendar.MONTH, 12);
+        fecha.set(GregorianCalendar.DATE, 1);
+        usuario.setFechaNacimiento(fecha);
+
+        edad = g.obtenerEdad(usuario);
+        assertEquals(0,edad);
     }
 
 }
